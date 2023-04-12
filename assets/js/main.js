632eebc66cae7c72b52ce101c4f9fc2c47918884
anra54 <anugrah.ramadhani@gmail.com>
2023-04-12 16:48:46 +0700

    feat: improve content rating

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,3 +152,15 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+// Set this to the width of one star.
+var starWidth = 20;
+
+$.fn.stars = function() {
+  return $(this).each(function() {
+    $(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat($(this).html())))) * starWidth));
+  });
+}
+$(document).ready(function() {
+  $('span.stars').stars();
+});
